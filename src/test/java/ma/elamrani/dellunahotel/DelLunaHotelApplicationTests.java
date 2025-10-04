af03463410c0cd32fb842399f8c25a35ddca0a20
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-10-04 17:33:46 +0100

    Initial commit

---
+++ src/test/java/ma/elamrani/dellunahotel/DelLunaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.elamrani.dellunahotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelLunaHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
