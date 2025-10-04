35ee4e64b97b9d7e1c421b9d17d48a8cfadfaf8c
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-10-04 17:41:04 +0100

    feat: add BookingRepository, RoomRepository and UserRepository.

---
+++ src/main/java/ma/elamrani/dellunahotel/repository/BookingRepository.java
@@ -0,0 +1,12 @@
+package ma.elamrani.dellunahotel.repository;
+
+import ma.elamrani.dellunahotel.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
+
+}
